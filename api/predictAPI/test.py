906221b96b23af6fac3f35ccf1c41e017d236010
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2024-02-12 10:30:02 +0530

    fix: updated models folder

--- api/predictAPI/test.py
+++ api/predictAPI/test.py
@@ -5,5 +5,5 @@ warnings.filterwarnings("ignore")
 
 
 model = joblib.load('./model/fsp_model.pkl');
-prediction = model.predict([[23.5, 23,5]]);
+prediction = model.predict([[2, 23,5]]);
 print(prediction)
